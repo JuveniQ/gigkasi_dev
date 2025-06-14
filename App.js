3374c33283f912327cae32772f70d1741fb7784e
SthembisoGit <sthembisosotsha@gmail.com>
2025-06-14 04:40:16 +0200

    removedc Ath screen

--- App.js
+++ App.js
@@ -121,7 +121,7 @@ function RootStack() {
       <Stack.Screen name='EditProfile' component={EditProfileScreen} />
       <Stack.Screen name="Notifications" component={NotificationsScreen} />
       <Stack.Screen name='PrivacySecurity' component={PrivacySecurityScreen} />
-      <Stack.Screen name='Auth' component={AuthScreen}/>
+      
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
